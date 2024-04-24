993d443ebba7ca8cc40ac5d1818ee47d92297bfd
Alallaf1 <parental.co122@gmail.com>
2024-04-24 23:02:23 +0300

    Correct the class name from CreatePool to CreatePoll

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -4,18 +4,18 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
-class CreatePool extends Component // created by this command (php artisan make:livewire CreatePool).
+class CreatePoll extends Component // created by this command (php artisan make:livewire CreatePoll).
 {
-    public $title; // all public properties of class CreatePool will be passed to the view bellow
+    public $title; // all public properties of class CreatePoll will be passed to the view bellow
 
     public $options = ['First'];
 
     public function render()
     {
-        return view('livewire.create-pool');
+        return view('livewire.create-poll');
     }
 
-    public function addOption() // an livewire action (is just a php function to add new empty element to optiions array)
+    public function addOption() // an livewire action (is just a php function to add new empety element to options array)
     {
         $this->options[] = ''; // $this->options[] to add this value ('') at the end of this array
     }
@@ -24,5 +24,10 @@ class CreatePool extends Component // created by this command (php artisan make:
     {
         unset($this->options[$index]); // to remove element from the array
         $this->options = array_values($this->options); // to re generete the indexes of the array (remove the gaps of deleted elements)
+    }
+
+    public function createPoll()
+    {
+        
     }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,14 +1,12 @@
 <div>
-    <form>
+    <form wire:submit.prevent="createPoll">
         <label>Poll Title</label>
   
-        <input type="text" wire:model.live="title" /> {{-- wire:model.live makes the model title(passed from CreatePool class) dynamiclly updating without page refreshing or using of JavaScript --}}
+        <input type="text" wire:model.live="title" /> {{-- wire:model.live makes the model title(passed from CreatePoll class) dynamiclly updating without page refreshing or using of JavaScript by ourself --}}
                                                     {{-- this wire:model.live its not part of html itt part of livewire sentacs the handel spetially on the client side--}}
-                                                    {{-- and this means to ask livewire to synchronize the value of title property (of CreatePool component class) on the server --}}
-                                                    {{-- so the livewire makes requests to the server to re render the view (of CreatePool component class) each time the title is updating on the client side --}}
+                                                    {{-- and this means to ask livewire to synchronize the value of title property (of CreatePoll component class) on the server --}}
+                                                    {{-- so the livewire makes requests to the server to re render the view (of CreatePoll component class) each time the title is updating on the client side --}}
                                                     {{-- then the javaScript (included in the livewire) making a smart update to the html of the view, and compare the part that changed with the old one, and it only update the new relevent part in a smart way --}}
-
-        Current title: {{ $title }} {{-- $title is a property that passed from the CreatePool Class --}}
         
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button> {{-- click.prevent will prevent the html behavior from submit the form with each click, insted call this livewire action (addOption) from the component class --}}
@@ -21,11 +19,12 @@
                 <div class="flex gap-2">
                     <input type="text" wire:model="options.{{ $index }}" /> {{-- to make the options.{{ $index }} (each index of the options array) wired --}}
                     <button class="btn"
-                        wire:click.prevent="removeOption({{ $index }})">Remove</button> {{-- prevent the submit and call the removeOption action with $indix paramiter from the CreatePool class --}}
+                        wire:click.prevent="removeOption({{ $index }})">Remove</button> {{-- prevent the submit and call the removeOption action with $indix paramiter from the CreatePoll class --}}
                 </div>
             </div>
             @endforeach
         </div>
-    
+        
+        <button type="submit" class="btn">Create Poll</button>
     </form>
 </div>
