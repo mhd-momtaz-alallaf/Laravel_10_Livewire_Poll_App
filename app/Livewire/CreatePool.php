5ffdc3bb4dc7cc61ff9ed3c7a5b53d990bdf927d
Alallaf1 <parental.co122@gmail.com>
2024-04-24 22:05:40 +0300

    start performing the CreatePool Livewire component by add an empte option with each click

--- app/Livewire/CreatePool.php
+++ app/Livewire/CreatePool.php
@@ -8,8 +8,15 @@ class CreatePool extends Component // created by this command (php artisan make:
 {
     public $title; // all public properties of class CreatePool will be passed to the view bellow
 
+    public $options = ['First'];
+
     public function render()
     {
         return view('livewire.create-pool');
+    }
+
+    public function addOption() // an livewire action (is just a php function to add new empty element to optiions array)
+    {
+        $this->options[] = ''; // $this->options[] to add this value ('') at the end of this array
     }
 }
--- resources/views/livewire/create-pool.blade.php
+++ resources/views/livewire/create-pool.blade.php
@@ -1,13 +1,26 @@
 <div>
     <form>
-      <label>Poll Title</label>
+        <label>Poll Title</label>
   
-      <input type="text" wire:model.live="title" /> {{-- wire:model.live makes the model title(passed from CreatePool class) dynamiclly updating without page refreshing or using of JavaScript --}}
+        <input type="text" wire:model.live="title" /> {{-- wire:model.live makes the model title(passed from CreatePool class) dynamiclly updating without page refreshing or using of JavaScript --}}
                                                     {{-- this wire:model.live its not part of html itt part of livewire sentacs the handel spetially on the client side--}}
                                                     {{-- and this means to ask livewire to synchronize the value of title property (of CreatePool component class) on the server --}}
                                                     {{-- so the livewire makes requests to the server to re render the view (of CreatePool component class) each time the title is updating on the client side --}}
                                                     {{-- then the javaScript (included in the livewire) making a smart update to the html of the view, and compare the part that changed with the old one, and it only update the new relevent part in a smart way --}}
 
         Current title: {{ $title }} {{-- $title is a property that passed from the CreatePool Class --}}
+        
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button> {{-- click.prevent will prevent the html behavior from submit the form with each click, insted call this livewire action (addOption) from the component class --}}
+        </div>
+      
+        <div>
+            @foreach ($options as $index => $option)
+            <div class="mb-4">
+                {{ $index }} - {{ $option }}
+            </div>
+            @endforeach
+        </div>
+    
     </form>
 </div>
