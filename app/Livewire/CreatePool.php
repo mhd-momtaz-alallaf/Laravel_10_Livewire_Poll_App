020092ace241ad8f40e02bb9a3cda1aa2a001e3d
Alallaf1 <parental.co122@gmail.com>
2024-04-24 21:45:14 +0300

    test the livewire component by add form with input that changing with typing

--- app/Livewire/CreatePool.php
+++ app/Livewire/CreatePool.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class CreatePool extends Component // created by this command (php artisan make:livewire CreatePool).
 {
+    public $title; // all public properties of class CreatePool will be passed to the view bellow
+
     public function render()
     {
         return view('livewire.create-pool');
--- resources/views/livewire/create-pool.blade.php
+++ resources/views/livewire/create-pool.blade.php
@@ -1,3 +1,13 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <form>
+      <label>Poll Title</label>
+  
+      <input type="text" wire:model.live="title" /> {{-- wire:model.live makes the model title(passed from CreatePool class) dynamiclly updating without page refreshing or using of JavaScript --}}
+                                                    {{-- this wire:model.live its not part of html itt part of livewire sentacs the handel spetially on the client side--}}
+                                                    {{-- and this means to ask livewire to synchronize the value of title property (of CreatePool component class) on the server --}}
+                                                    {{-- so the livewire makes requests to the server to re render the view (of CreatePool component class) each time the title is updating on the client side --}}
+                                                    {{-- then the javaScript (included in the livewire) making a smart update to the html of the view, and compare the part that changed with the old one, and it only update the new relevent part in a smart way --}}
+
+        Current title: {{ $title }} {{-- $title is a property that passed from the CreatePool Class --}}
+    </form>
 </div>
