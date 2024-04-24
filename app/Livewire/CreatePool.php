50fe97e5f15fa77066ed73a09fe2443566c74e84
Alallaf1 <parental.co122@gmail.com>
2024-04-24 20:21:58 +0300

    create (CreatePool) Livewire component

---
+++ app/Livewire/CreatePool.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreatePool extends Component // created by this command (php artisan make:livewire CreatePool).
+{
+    public function render()
+    {
+        return view('livewire.create-pool');
+    }
+}
